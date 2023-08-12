2efeaca1a4b4f7725eee7107db01ac542afe4a3b
Maria Kasian <124454348+mariakasian@users.noreply.github.com>
2023-08-12 22:58:41 +0400

    2nd version
    Розкоментувала в JsonPlaceholderTest 8 по 14 рядки

--- src/main/java/org/example/jsonplaceholder/JsonPlaceholderTest.java
+++ src/main/java/org/example/jsonplaceholder/JsonPlaceholderTest.java
@@ -5,12 +5,12 @@ import java.io.IOException;
 public class JsonPlaceholderTest {
     public static void main(String[] args) throws IOException {
         OpenTasksFetcher.fetchAndPrintOpenTasks(1);
-//        CommentFetcher.fetchAndSaveComments(2);
-//        JsonPlaceholderClient.getAllUsers();
-//        JsonPlaceholderClient.getUserById(1);
-//        JsonPlaceholderClient.getUserByUsername("Antonette");
-//        JsonPlaceholderClient.updateExistingUser(1);
-//        JsonPlaceholderClient.deleteUser(10);
-//        JsonPlaceholderClient.createNewUser();
+        CommentFetcher.fetchAndSaveComments(2);
+        JsonPlaceholderClient.getAllUsers();
+        JsonPlaceholderClient.getUserById(1);
+        JsonPlaceholderClient.getUserByUsername("Antonette");
+        JsonPlaceholderClient.updateExistingUser(1);
+        JsonPlaceholderClient.deleteUser(10);
+        JsonPlaceholderClient.createNewUser();
     }
 }
